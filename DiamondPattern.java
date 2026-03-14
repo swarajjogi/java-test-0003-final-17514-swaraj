13cf9f22c36d85a0dd6c9acf61a9b25c8b808260
Swaraj Jogi <swarajjogi@gmail.com>
2026-03-14 13:09:29 +0530

    Added

---
+++ DiamondPattern.java
@@ -0,0 +1,22 @@
+    public class DiamondPattern {
+        public static void main(String[] args) {
+            int n = 5;
+
+            for (int i = 1; i <= n; i++) {
+        
+                for (int space = 0; space < n - i; space++) {
+                    System.out.print("  ");
+                }
+
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+
+                for (int j = i - 1; j >= 1; j--) {
+                    System.out.print(j + " ");
+                }
+
+                System.out.println();
+            }
+        }
+    }
